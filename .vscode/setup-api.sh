fb6c70ddcef2929d5dfcc8fca26aa10d35cb4be5
Daniel Jurek <djurek@microsoft.com>
2022-10-12 21:14:04 +0000

    Setup API and Web separately

--- .vscode/setup-api.sh
+++ .vscode/setup-api.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function portUrl() {
+function codespacesPortUrl() {
     local portNumber=$1
     gh codespace ports \
         -c "$CODESPACE_NAME" \
@@ -8,17 +8,19 @@ function portUrl() {
         --jq "map(select(.sourcePort == $portNumber))[0].browseUrl"
 }
 
-if [ "$CODESPACES" = 'true' ]; then     # Running in codespaces
+if [ "$CODESPACES" = 'true' ]; then
     echo "Running in Codespaces. Setting port configurations."
 
-    webPortUrl=$(portUrl 3000)
-    echo "azd env set REACT_APP_WEB_BASE_URL \"$webPortUrl\""
-    azd env set REACT_APP_WEB_BASE_URL "$webPortUrl"
-
-    apiPortUrl=$(portUrl 3100)
+    apiPortUrl=$(codespacesPortUrl 3100)
     echo "azd env set REACT_APP_API_BASE_URL \"$apiPortUrl\""
     azd env set REACT_APP_API_BASE_URL "$apiPortUrl"
 
     echo "Setting API port to public so web app can access it" 
     gh codespace ports visibility 3100:public -c "$CODESPACE_NAME"
+
+else 
+    echo "Running in local development mode. Setting port configurations"
+    
+    echo "azd env set REACT_APP_API_BASE_URL \"http://localhost:3100\""
+    azd env set REACT_APP_API_BASE_URL "http://localhost:3100"
 fi
