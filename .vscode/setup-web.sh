efe13e3756b879621eac1eae8990789500c3ceb0
Daniel Jurek <djurek@microsoft.com>
2022-10-14 15:10:08 +0000

    Set WDS_SOCKET_PORT

--- .vscode/setup-web.sh
+++ .vscode/setup-web.sh
@@ -15,9 +15,19 @@ if [ "$CODESPACES" = 'true' ]; then
     echo "azd env set REACT_APP_WEB_BASE_URL \"$webPortUrl\""
     azd env set REACT_APP_WEB_BASE_URL "$webPortUrl"
 
+    # Set port for websocket debugging so the application can communicate with
+    # Webpack DevServer running in codespaces
+    # https://github.com/community/community/discussions/11524
+    echo "azd env set WDS_SOCKET_PORT \"443\""
+    azd env set WDS_SOCKET_PORT "443"
+
 else 
     echo "Running in local development mode. Setting port configurations"
     
     echo "azd env set REACT_APP_WEB_BASE_URL \"http://localhost:3000\""
     azd env set REACT_APP_WEB_BASE_URL "http://localhost:3000"
+
+    # Set Webpack DevServer configuration to default for local development
+    echo "azd env set WDS_SOCKET_PORT \"\""
+    azd env set WDS_SOCKET_PORT ""
 fi
